8fd8fc7ae3005115544896b9b2b3fd73edfe461b
Rajil Bajracharya <imrb1213@gmail.com>
2022-11-05 15:50:18 +0545

    Use mongodb for data persistency

--- routes/genres.ts
+++ routes/genres.ts
@@ -1,23 +1,26 @@
 import express, { Request, Response } from 'express';
 import Joi from 'joi';
+import mongoose, { mongo } from 'mongoose';
 
 const router: express.Router = express.Router();
 
-type genreType = {
-    id: number; 
-    name: string
-};
+// setup db: use vidly
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log("Successfully connected to MongoDB"))
+    .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-// sample list of genres, to be replaced with call to DB later...
-const genres: genreType[] = [
-    {id: 1, name: "Horror"},
-    {id: 2, name: "Humor"},
-    {id: 3, name: "Romance"},
-    {id: 4, name: "Thriller"},
-    {id: 5, name: "Suspense"},
-    {id: 6, name: "Erotica"},
-    {id: 7, name: "Fantasy"}
-]
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minlength :3,
+        required: true
+    }
+})
+
+// setup collection: Genre = db.genres
+const Genre = mongoose.model('genres', genreSchema);
+
+///// TODO: sample list of genres, to be replaced with call to DB later...
 
 const validateBody = (body: {}): Joi.ValidationResult => {
     const schema: Joi.ObjectSchema = Joi.object({
@@ -27,40 +30,44 @@ const validateBody = (body: {}): Joi.ValidationResult => {
     return schema.validate(body);
 }
 
-const isValidId = (val: any): [boolean, genreType] => {
-    const emptyGenre = {id: 0, name: ''};
+const isValidId = (id: any): boolean => {
+    const emptyGenre = {_id: 0, name: ''};
 
-    let id = Number(val).valueOf();
-    if (!id) return [false, emptyGenre];
+    let genre = Genre.findOne({_id: id});
+    if (!genre) return false
 
-    let genre = genres.find((el: genreType) => el.id === id);
-    if (!genre) return [false, emptyGenre];
-
-    return [true, genre];
+    return true;
 }
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
+    const genres = await Genre.find().select({name: 1, _id: 0});
     return res.send(genres);
 })
 
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
     const { error } = validateBody(req.body);
 
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
 
-    let genre: genreType = {
-        id: genres.length + 1,
-        name: req.body.name
-    }
-    genres.push(genre);
+    const name = req.body.name
+    const genre = new Genre({
+        name: name
+    })
 
-    res.send(genre);
+    try{
+        const result = await genre.save();
+        res.send(result);
+    }
+    catch (err: any) {
+        res.send(err.message);
+
+    }
 })
 
-router.put('/:id', (req: Request, res: Response) => {
-    let [result, genre] = isValidId(req.params.id);
+router.put('/:id', async (req: Request, res: Response) => {
+    let result = isValidId(req.params.id);
 
     if (!result) return res.status(400).send(`${req.params.id} is an invalid id`)
 
@@ -70,18 +77,20 @@ router.put('/:id', (req: Request, res: Response) => {
         return res.status(400).send(error.details[0].message);
     }
 
-    genre.name = req.body.name;
-    res.send(genre);
+    const name = req.body.name;
+    const dbResult = await Genre.findByIdAndUpdate(req.params.id, {
+        $set: { name: name}
+    })
+    res.send(dbResult);
 })
 
-router.delete('/:id', (req: Request, res: Response) => {
-    let [result, genre] = isValidId(req.params.id);
+router.delete('/:id', async (req: Request, res: Response) => {
+    let result = isValidId(req.params.id);
     if (!result) return res.status(400).send(`${req.params.id} is an invalid id`)
 
-    let idxToRemove: number = genres.indexOf(genre);
-    genres.splice(idxToRemove, 1);
+    const dbResult = await Genre.findByIdAndDelete(req.params.id);
 
-    res.send(genre);
+    res.send(dbResult);
 })
 
 export default router;
