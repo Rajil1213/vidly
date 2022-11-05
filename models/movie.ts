596051791afa9a5ffc5e1ba96032f754639c694a
Rajil Bajracharya <imrb1213@gmail.com>
2022-11-06 00:47:27 +0545

    Create the movie schema

---
+++ models/movie.ts
@@ -0,0 +1,51 @@
+import Joi from 'joi';
+import mongoose from "mongoose";
+
+// define fields from the Genre schema that are required here
+const HybridSchema = new mongoose.Schema({
+    name: String
+})
+
+interface customerInterface {
+    title: string;
+    numberInStock: number;
+    dailyRentalRate: number;
+    genre: typeof HybridSchema;
+}
+
+export const Movie = new mongoose.Schema({
+    title: {
+        type: String,
+        minlength: 2,
+        maxlength: 50,
+        required: true
+    },
+    numberInStock: {
+        type: Number,
+        default: 0
+    },
+    dailyRentalRate: {
+        type: Number,
+        // default to 0 if not in stock, else 100
+        default: function (this: customerInterface) {
+            if (this.numberInStock > 0) return 0;
+            return 100;
+        }
+    },
+    genre: {
+        type: HybridSchema,
+        required: true
+    }
+})
+
+export const validateBody = (body: {}): Joi.ValidationResult => {
+    const schema: Joi.ObjectSchema = Joi.object({
+        title: Joi.string().min(2).max(50).required(),
+        numberInStock: Joi.number().required(),
+        dailyRentalRate: Joi.number().required(),
+        // for name
+        genre: Joi.string().min(3).required()
+    });
+
+    return schema.validate(body);
+}
