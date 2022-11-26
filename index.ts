1b9ffbf290a6149d2537c5e31c59010b97582041
Rajil Bajracharya <imrb1213@gmail.com>
2022-11-26 19:56:59 +0545

    Refactor routes

--- index.ts
+++ index.ts
@@ -1,17 +1,14 @@
 import config from 'config';
-import express, {  Request, Response } from 'express';
+import express from 'express';
 import 'express-async-errors';
-import { default as genres } from './routes/genres';
-import { default as customers } from './routes/customers';
-import { default as movies } from './routes/movies';
-import { default as rentals } from './routes/rentals';
-import { default as register } from './routes/users';
-import { default as auth } from './routes/auth';
 import mongoose from 'mongoose';
-import { errorHandler } from './middleware/error';
 import { logger } from './util/logger';
 
 const app: express.Application = express();
+
+// setup routes
+import routes from './startup/routes'
+routes(app);
 
 if (!config.get('jwtPrivateKey')) {
     console.error("FATAL ERROR: jwtPrivateKey is not defined.");
@@ -28,19 +25,6 @@ process.on('unhandledRejection', (ex: any) => {
     process.exit(1);
 })
 
-// get POST body (middleware); req.body = undefined without this
-app.use(express.json());
-// for all routes starting with `/api/genres` use `genres` (router) as the handler
-app.use('/api/genres', genres);
-// similarly,
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', register);
-app.use('/api/auth', auth);
-
-// add error middleware
-app.use(errorHandler);
 
 // setup db: use vidly
 // for single-replicaSet, set `directConnection=true` to
---
+++ startup/routes.ts
@@ -0,0 +1,27 @@
+import express, { Application } from "express";
+import { errorHandler } from "../middleware/error";
+import { default as genres } from '../routes/genres';
+import { default as customers } from '../routes/customers';
+import { default as movies } from '../routes/movies';
+import { default as rentals } from '../routes/rentals';
+import { default as register } from '../routes/users';
+import { default as auth } from '../routes/auth';
+
+// for all routes starting with `/api/genres` use `genres` (router) as the handler
+const routes = (app: Application) => {
+    // get POST body (middleware); req.body = undefined without this
+    app.use(express.json());
+    // for all routes starting with `/api/genres` use `genres` (router) as the handler
+    app.use('/api/genres', genres);
+    // similarly,
+    app.use('/api/customers', customers);
+    app.use('/api/movies', movies);
+    app.use('/api/rentals', rentals);
+    app.use('/api/users', register);
+    app.use('/api/auth', auth);
+
+    // add error middleware
+    app.use(errorHandler);
+}
+
+export default routes;
