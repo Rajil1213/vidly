c88b7bd0ce4151e1e8795831ef7eca09658bafce
Rajil Bajracharya <imrb1213@gmail.com>
2022-11-27 23:21:40 +0545

    Test the auth middleware

---
+++ tests/integration/auth.test.ts
@@ -0,0 +1,48 @@
+import request from 'supertest';
+import { Genre } from '../../models/genre';
+import { User } from '../../models/user';
+
+describe('auth middleware', () => {
+    let server: any;
+    beforeEach(async () => { 
+        const mod = await import('../../index')
+        server = (mod as any).default;
+    });
+    afterEach(async () => {
+        if (server) {
+            server.close();
+        }
+        // Cleanup for valid POST request
+        await Genre.deleteMany({});
+    });
+
+    // Happy Path
+    let token: string
+    const exec =  () => {
+        return request(server)
+        .post('/api/genres')
+        .set('x-auth-token', token)
+        .send({name: "valid"})
+    }
+
+    beforeEach(() => {
+        token = new User().generateAuthToken();
+    })
+
+    it('should return 401 if no token is provided', async () => {
+        token = "";
+        const res = await exec();
+        expect(res.status).toBe(401);
+    })
+
+    it('should return 400 if invalid token is provided', async () => {
+        token = "a";
+        const res = await exec();
+        expect(res.status).toBe(400);
+    })
+
+    it('should return 200 if token is valid', async () => {
+        const res = await exec();
+        expect(res.status).toBe(200);
+    })
+})
