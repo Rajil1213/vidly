06aa6014d80ab5bcdb99c806fce123136d737f56
Rajil Bajracharya <imrb1213@gmail.com>
2022-11-28 01:42:00 +0545

    Refactor

--- tests/integration/returns.test.ts
+++ tests/integration/returns.test.ts
@@ -5,16 +5,10 @@ import { User } from '../../models/user';
 
 describe('/api/returns', () => {
     let server: any;
-    let customerId: mongoose.Types.ObjectId;
-    let movieId: mongoose.Types.ObjectId;
     let rental: any;
-    let token: string;
     beforeEach(async () => {
         const mod = await import('../../index')
         server = (mod as any).default;
-        customerId = new mongoose.Types.ObjectId();
-        movieId = new mongoose.Types.ObjectId();
-        token = new User().generateAuthToken();
         rental = new Rental({
             customer: {
                 _id: customerId, 
@@ -37,23 +31,39 @@ describe('/api/returns', () => {
         await Rental.deleteMany({});
     });
 
-    it('should return 401 if the client is not logged in', async () => {
-        const res = await request(server)
+    // Happy path
+    let token: string;
+    let customerId: mongoose.Types.ObjectId;
+    let movieId: mongoose.Types.ObjectId;
+    let body: any;
+    const exec = () => {
+        return request(server)
             .post('/api/returns')
-            .send({
-                customerId,
-                movieId
-            })
+            .set('x-auth-token', token)
+            .send(body)
+    }
+
+    beforeEach(() => {
+        customerId = new mongoose.Types.ObjectId();
+        movieId = new mongoose.Types.ObjectId();
+        token = new User().generateAuthToken();
+    })
+
+    it('should return 401 if the client is not logged in', async () => {
+        body = {
+            customerId,
+            movieId
+        }
+        token = ""
+        const res = await exec();
         expect(res.status).toBe(401);
     })
 
     it('should return 400 if customerId is not provided', async () => {
-        const res = await request(server)
-            .post('/api/returns')
-            .set('x-auth-token', token)
-            .send({
-                movieId
-            })
+        body = {
+            movieId
+        }
+        const res = await exec();
         expect(res.status).toBe(400);
     })
 })
