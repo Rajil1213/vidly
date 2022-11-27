220c49fe19e1dcfdc59e3a764726ef00bf20baea
Rajil Bajracharya <imrb1213@gmail.com>
2022-11-28 04:12:11 +0545

    Use request validator middleware

--- routes/returns.ts
+++ routes/returns.ts
@@ -5,15 +5,13 @@ import { admin } from '../middleware/admin';
 import { validateObjectId } from '../middleware/validateObjectId';
 import { Rental, validateBody } from '../models/rental';
 import { Movie } from '../models/movie';
+import { validateReq } from '../middleware/validateReq';
 
 const router: express.Router = express.Router();
 
 ///// TODO: sample list of genres, to be replaced with call to DB later...
 
-router.post('/', [auth], async (req: Request, res: Response) => {
-    const { error } = validateBody(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
-
+router.post('/', [auth, validateReq(validateBody)], async (req: Request, res: Response) => {
     const { customerId, movieId } = req.body;
     const rental = await Rental.findOne({
         "customer._id": customerId,
@@ -26,13 +24,13 @@ router.post('/', [auth], async (req: Request, res: Response) => {
 
     rental.dateReturned = new Date(Date.now());
     rental.setRentalFee();
+    await rental.save();
 
     const movie = await Movie.findById(movieId);
     if (movie) {
         movie.numberInStock++;
         movie.save()
     }
-    await rental.save();
 
     return res.status(200).send(rental);
 })
