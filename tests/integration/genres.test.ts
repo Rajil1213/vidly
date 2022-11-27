14359170952c6abe47b7d5882d407489e0aba5e7
Rajil Bajracharya <imrb1213@gmail.com>
2022-11-27 21:15:08 +0545

    Add test for /api/genres/:id

--- tests/integration/genres.test.ts
+++ tests/integration/genres.test.ts
@@ -1,13 +1,14 @@
+import mongoose from 'mongoose';
 import request  from 'supertest';
 import { Genre } from '../../models/genre';
 
 describe('/api/genres', () => {
     let server: any;
-    beforeAll(async () => {
+    beforeEach(async () => {
         const mod = await import('../../index')
         server = (mod as any).default;
     });
-    afterAll(async () => {
+    afterEach(async () => {
         if (server) {
             server.close();
         }
@@ -26,6 +27,25 @@ describe('/api/genres', () => {
             expect(res.status).toBe(200); 
             expect(res.body.some((g: {name: string}) => g.name === "genre1")).toBeTruthy()
             expect(res.body.some((g: {name: string}) => g.name === "genre2")).toBeTruthy()
+        })
+    })
+
+    describe('GET /:id', () => {
+        it('should return a valid genre for given id', async () => {
+            // populate
+            const testGenre = {_id: new mongoose.Types.ObjectId(), name: "genre1"}
+            await Genre.collection.insertOne(testGenre)
+            const res = await request(server).get(`/api/genres/${testGenre._id}`);
+            expect(res.status).toBe(200);
+            expect(res.body).toMatchObject(testGenre);
+        })
+        it('should return 404 if invalid id is passed', async () => {
+            // invalid format
+            const res_invalid = await request(server).get("/api/genres/1")
+            expect(res_invalid.status).toBe(404);
+            // valid format, invalid ID
+            const res_valid = await request(server).get(`/api/genres/${new mongoose.Types.ObjectId()}`)
+            expect(res_valid.status).toBe(404);
         })
     })
 })
