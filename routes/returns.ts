4a17f17eae1441c0506435fcb960e302c549f7aa
Rajil Bajracharya <imrb1213@gmail.com>
2022-11-28 01:37:41 +0545

    Make test#2 pass

--- routes/returns.ts
+++ routes/returns.ts
@@ -3,14 +3,15 @@ import express, { NextFunction, Request, Response } from 'express';
 import { auth } from '../middleware/auth';
 import { admin } from '../middleware/admin';
 import { validateObjectId } from '../middleware/validateObjectId';
-import { Rental } from '../models/rental';
+import { Rental, validateBody } from '../models/rental';
 
 const router: express.Router = express.Router();
 
 ///// TODO: sample list of genres, to be replaced with call to DB later...
 
-router.post('/', async (req: Request, res: Response) => {
-    return res.status(401).send('Unauthorized access')
+router.post('/', [auth], async (req: Request, res: Response) => {
+    const { error } = validateBody(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
 })
 
 
