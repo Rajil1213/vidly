179ab49ca65faab732103ae0821fbf0554d3bb93
Rajil Bajracharya <imrb1213@gmail.com>
2022-12-03 21:40:11 +0545

    Fix typo

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 export const auth = (req: Request, res: Response, next: NextFunction) => {
     const token = req.header('x-auth-token');
     if (!token) {
-        return res.status(401).send("Access denied. Not token provided.")
+        return res.status(401).send("Access denied. No token provided.")
     }
     try {
         const decoded = jwt.verify(token, config.get("jwtPrivateKey"))
